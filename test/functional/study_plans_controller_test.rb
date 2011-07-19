ba706a53cdf6d95d915d278b88e4cd11fc032a94
Karmen Batista <karmen.batista@gmail.com>
2011-07-18 19:49:58 -0700

    Controller Test

--- test/functional/study_plans_controller_test.rb
+++ test/functional/study_plans_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class StudyPlansControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    assert @user = Factory.create(:user)
+  end
+  
+  test "should get index" do
+    sign_in_with_role @user, :admin
+    get :index
+    assert_response  :success
+    assert_not_nil assigns(:study_plans)
   end
 end
